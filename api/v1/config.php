ab8e7d698aa2b52459f2474113ada0aca78e4f98
cmittle <cory.mittleider@gmail.com>
2015-10-09 23:00:47 -0500

    Changed index.html to add Bootstrap sidebar on the left side of page. added css/simple-sidebar.css Embedded ng-view inside of page wrapper. Intend to used nav menus in left side bar to control UI router.  TODO

--- api/v1/config.php
+++ api/v1/config.php
@@ -4,7 +4,8 @@
  */
 define('DB_USERNAME', 'corysc5_wind2');
 define('DB_PASSWORD', 'cm259925');
-define('DB_HOST', 'localhost');
+//UPLOAD CHANGE define('DB_HOST', 'localhost');
+define('DB_HOST', '205.134.253.65');
 define('DB_NAME', 'corysc5_wind2');
 
 ?>
