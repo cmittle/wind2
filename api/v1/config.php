25dabb41d79b5a73fb3ca8a79a2ec5ab0c1ea0f8
cmittle <cory.mittleider@gmail.com>
2015-10-10 00:01:24 -0500

    Minor tweaks. I haven't figured out how to connect Netbeans to hosted database yet.

--- api/v1/config.php
+++ api/v1/config.php
@@ -4,8 +4,10 @@
  */
 define('DB_USERNAME', 'corysc5_wind2');
 define('DB_PASSWORD', 'cm259925');
-//UPLOAD CHANGE define('DB_HOST', 'localhost');
-define('DB_HOST', '205.134.253.65');
+define ('DB_HOST', 'localhost');
+//have to set up database connection in Netbeans as well as add house IP 
+//address to "remote MySQL on hosting cpanel
+//This still doesn't connect from NetBeans, have to figure this part out...
 define('DB_NAME', 'corysc5_wind2');
 
 ?>
