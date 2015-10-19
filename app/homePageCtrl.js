f179d43ea0daac07d145724881469d48617a0a88
cmittle <cory.mittleider@gmail.com>
2015-10-18 23:12:03 -0500

    Gearbox sidebar menu fully functional.  Clicking on gearbox label hides all gearbox mfg's and clicking on gb mfg expands/collapses model list of that gb mfg.

--- app/homePageCtrl.js
+++ app/homePageCtrl.js
@@ -4,6 +4,8 @@ $scope.concatGbArray = []; //empty array to hold concatenated data of gb mfg and
 
 $scope.gbMfgArray = [];  //move to here once I get the ng-repeat to work
 $scope.gbModelArray = [];  //load all of the gb model numbers at once then filter under each list item
+$scope.showGbMfgList = false;  //this is used to show/hide the list of gb mfgs below the "Gearbox" heading
+
 
 /* DELETE these variables and functions they were only used for testing/implemntation
 $scope.strict = true;
@@ -31,15 +33,19 @@ $scope.showGbModels = function(x) {
 	//this function takes the gbMfgId from the clicked mfg name on the page and changes the "view" property of that gb model to true if the gbmfg for that model matches
 			//eg if gbMfg Hansen is selected (id = 5) then all models that list Hansen (id=5) in the mfg property have the view property changed to true
 	angular.forEach($scope.gbModelArray, function (ea, key) {
-	//this loops 
 		if (ea.mfg == x) {
-			ea.view = true;
-		} else {
-			ea.view = false;
+			//This toggles the view property of the list of models under the gb mfg just clicked
+			//doing it this way allows several or no gb mfg list of models to be shown at one time.
+			ea.view = !ea.view;
 		};
 	});
 	
 	
+};
+
+$scope.toggleGbMfgList = function () {
+	//This is used to toggle showGbMfgList variable
+	$scope.showGbMfgList = !$scope.showGbMfgList;	
 };
 
 /*Data.get('gearbox_basic').then(function(data){
@@ -115,7 +121,7 @@ $scope.getgbbasic = function() {
 		          angular.forEach (data.data, function (value, key){
 		          	//this creates a temporary object with concatenated mfg ID and mfg Name
 		          	//I will use this concatentated informaiton to create a sorted object which can be tied to the pulldown menu on this page
-		          	$scope.gbObject= {id: (value['id']), name:(value['mfg'])}; // + ' ' + value['model']
+		          	$scope.gbObject= {id: (value['id']), name:(value['mfg'])}; // + ' ' + value['model'], view:(true)
 		          	//add new gb to array in forEach loop to gb master list to be sorted.
 		          	$scope.concatGbArray = $scope.concatGbArray.concat($scope.gbObject);
 		          	//console.log($scope.gbObject);
