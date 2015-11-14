4977197101c837b58df6b074a1fe78354d73e8d7
cmittle <cory.mittleider@gmail.com>
2015-11-14 00:01:37 -0600

    added $http POST functionality to bearingSpecificsCtrl so I can edit records

--- app/bearingSpecificsCtrl.js
+++ app/bearingSpecificsCtrl.js
@@ -52,7 +52,7 @@ app.controller('bearingSpecificsCtrl', function ($scope, $modal, $filter, Data) 
 });
 
 
-app.controller('bearingSpecificsEditCtrl', function ($scope, $modalInstance, item, Data) {
+app.controller('bearingSpecificsEditCtrl', function ($scope, $modalInstance, item, $http, Data) {
 
   $scope.product = angular.copy(item);
   $scope.submitted = false;
@@ -71,7 +71,29 @@ app.controller('bearingSpecificsEditCtrl', function ($scope, $modalInstance, ite
             product.uid = $scope.uid;
             $scope.submitted = true; //this sets this to true so that the submit button can be turned in to a loading icon while this process to prevent duplicate submissions
             if(product.specific_id > 0){ // this is true if this editing a current product
-                Data.put('bearing_specifics/'+product.specific_id, product).then(function (result) {
+                $http({
+			    method: 'POST',
+			    url: 'api/v1/bearingsEdit.php',
+			    data: product
+		     }).then(function (data) {
+		          //product.description = $scope.testVar; //update the view after database update is successful
+		          var x = angular.copy(product);//copy current product with new information and send back to function that opened modal to update view
+		          x.save = 'update';
+		          $scope.submitted = false; //set back to false to show submit button again
+		          $modalInstance.close(x); //close Edit modal when completed
+		          
+		          console.log("SUCCESS  $scope.product = ");
+		          console.log($scope.product);
+		          
+		     }) .catch(function (data) {
+		     	console.log(data.data);
+		     	console.log("FAILED");
+	     	}); 
+                
+                
+                
+                
+                /*Data.put('bearing_specifics/'+product.specific_id, product).then(function (result) {
                     if(result.status != 'error'){
                         var x = angular.copy(product);
                         x.save = 'update';
@@ -80,7 +102,7 @@ app.controller('bearingSpecificsEditCtrl', function ($scope, $modalInstance, ite
                     }else{
                         console.log(result);
                     }
-                });
+                });*/
             }else{  //this is where it goes for a new product
             	console.log(product);
                 //product.status = 'Active';
